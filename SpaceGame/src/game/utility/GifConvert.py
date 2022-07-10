dd97130b0eb42c61c2e37cb1682648d12114b5f5
BillyTheSquid21 <97798337+BillyTheSquid21@users.noreply.github.com>
2022-07-10 20:43:28 +0100

    finished script to convert gifs

--- SpaceGame/src/game/utility/GifConvert.py
+++ SpaceGame/src/game/utility/GifConvert.py
@@ -1,3 +1,5 @@
+from os import listdir
+from os.path import isfile, join
 from PIL import Image
 from PIL import GifImagePlugin
 from PIL.PngImagePlugin import PngInfo
@@ -6,15 +8,31 @@ from dataclasses import dataclass
 #Script to turn gif files (eg from the pokemon showdown archive) into compatible pngs
 #Turns the gif into an image strip, with a saved metadata tag with the original frame count
 
+PATH_TO_GIF_FOLDER = "C:\\Users\\yoshi\\Downloads\\test\\gifs\\"
+PATH_TO_PNG_FOLDER = "C:\\Users\\yoshi\\Downloads\\test\\pngs\\"
+SHOW_PROGRESS = True
+
 @dataclass
 class CreatedImage:
     image : Image
-    frames : int = 0
+    frames : int = 0                    
+
+#Get all files in the gif directory
+def get_Directory_Paths(path):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    return files;
 
 #Get the frames and process
 def export_Gif_Frames(pathToGif, pathToPng):
-    createdImg = get_Gif_Frames(pathToGif)
-    save_Final_Image(createdImg.frames, createdImg.image, pathToPng)
+    files = get_Directory_Paths(pathToGif)
+    for file in range(0,len(files)):
+        gifPath = pathToGif+files[file]
+        createdImg = get_Gif_Frames(gifPath)
+        pngName = files[file].split('.')[0]
+        pngPath = pathToPng+pngName+".png"
+        save_Final_Image(createdImg.frames, createdImg.image, pngPath)
+        if (SHOW_PROGRESS):
+            print("Image " + str(file+1) + " of " + str(len(files)))
 
 #Gets frames from gif file with count
 def get_Gif_Frames(path):
@@ -62,7 +80,7 @@ def save_Final_Image(framecount, targetImage, path):
 #Main
 def main():
     print("Starting process...")
-    export_Gif_Frames("C:\\Users\\yoshi\\Downloads\\torterra.gif", "C:\\Users\\yoshi\\Downloads\\torterra2.png")
+    export_Gif_Frames(PATH_TO_GIF_FOLDER, PATH_TO_PNG_FOLDER)
 
 if __name__ == "__main__":
     main()
